a0bfefad0afa842eefb19eca995152f197feb4f9
foxt451 <svpetrenko123@gmail.com>
2022-04-25 17:15:16 +0300

    FIIINAAALY

--- accounts/adapter.py
+++ accounts/adapter.py
@@ -1,12 +1,19 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from webpage import models
+from django.utils.translation import gettext_lazy as _
 from allauth.account.models import EmailAddress
 from django.urls import reverse
+from django.utils.safestring import SafeString
 from django.shortcuts import redirect
 
 
 class MyAccountAdapter(DefaultAccountAdapter):
+    def __init__(self, request=None):
+        super().__init__(request)
+        # popover_str = _("If you believe this is a mistake or simply don't remember your password then go to Sign In page and follow 'Forgot Password' button.")
+        self.error_messages['email_taken'] = _('A user is already registered with this e-mail address. If you believe this is a mistake or simply don\'t remember your password then go to Login page and follow \'Forgot Password\' button.') # SafeString(_('A user is already registered with this e-mail address.') + f' <i class="fa fa-question-circle" data-bs-toggle="tooltip" title="{popover_str}"></i>')
+    
     def get_login_redirect_url(self, request):
         if not hasattr(request.user, 'workoutprofile'):
             # create an empty profile for the user and redirect to edit it
